9d1db7bfa00ccab2b7d45560b387089200d9409b
Bogdan Balan <balan.bogdan@gmail.com>
2021-02-07 10:14:17 +0000

    Init

---
+++ Source/TreeCity/TreeCityGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TreeCityGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TREECITY_API ATreeCityGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
